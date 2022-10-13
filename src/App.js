c99f9b904d4c189662e621d572f1cd375e0cc670
Harsh_1337 <56146951+Harsh-1337@users.noreply.github.com>
2022-10-13 13:08:48 +0530

    path bug

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Header />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="./" element={<Home />} />
           <Route path="./stack" element={<Stack />} />
           <Route path="./queue" element={<Queue />} />
           <Route path="./bst" element={<BST />} />
